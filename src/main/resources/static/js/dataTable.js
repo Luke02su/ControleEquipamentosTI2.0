b435382b14cea7753fbf7aa2653a96ac7b9805aa
Lucas <lucassamuel.ls29@gmail.com>
2025-10-05 21:57:48 -0300

    update

---
+++ src/main/resources/static/js/dataTable.js
@@ -0,0 +1,26 @@
+var table = $(document).ready(function () {
+$('.data-table').DataTable({
+    language: {
+        url: "//cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json"
+    },
+    lengthChange: true,      
+    searching: true,      
+    scrollY: "58vh",            
+    scrollCollapse: true,    
+    paging: true,     
+    fixedHeader: {
+        header: true, 
+        footer: true 
+    }, 
+    pageLength: 10,            
+    responsive: true 
+    });
+});
+
+$(document).ready(function() {
+    $('.select2').select2({
+        placeholder: "Selecione o equipamento",
+        allowClear: true,
+        width: '100%'
+    });
+});
